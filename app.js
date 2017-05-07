7d22dd890d25b5cfa1a8851a48137ee03e744d76
quocbao <quocbao747@gmail.com>
2017-05-07 20:44:28 +0700

    Update getService() in dao.js

--- app.js
+++ app.js
@@ -9,26 +9,9 @@ app.set('views', __dirname + '/views');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
-controller(app);
-
-
 // Server
 app.listen(3000, function(){
 	console.log("[INFO] Server started at localhost:3000");
 });
 
-
-var mongoose = require('mongoose')
-    , Admin = mongoose.mongo.Admin;
-
-/// create a connection to the DB
-var connection = mongoose.createConnection(
-    'mongodb://nvh_user:123456@ds131511.mlab.com:31511/nvh_test');
-connection.on('open', function() {
-    // connection established
-    new Admin(connection.db).listDatabases(function(err, result) {
-        console.log('listDatabases succeeded');
-        // database list stored in result.databases
-        var allDatabases = result.databases;
-    });
-});
+controller(app);
--- database/dao.js
+++ database/dao.js
@@ -6,20 +6,38 @@ var dao = {
 	// + Password:  32jJrkxwc&7F74#
 	// DB name:     simple-api-server
 	// DB username: quocbao
-	// DB password: Gz$3@RHtGR572S2
-	connString: 'mongodb://quocbao:Gz$3@RHtGR572S2@ds127101.mlab.com:27101/simple-api-server',
-	//connString: '"'mongodb://<dbuser>:<dbpassword>@ds127101.mlab.com:27101/simple-api-server',
-
+	// DB password: NX6cN2f55Mvaxrk
+	//connString: 'mongodb://nvh_user:123456@ds131511.mlab.com:31511/nvh_test',
+	dbName: 'simple-api-server',
+	dbUser: 'quocbao',
+	dbPass: 'NX6cN2f55Mvaxrk',
+	connString: 'mongodb://quocbao:NX6cN2f55Mvaxrk@ds127101.mlab.com:27101/simple-api-server',
 
 	connect: function()
 	{
+		console.log('[INFO] Connecting to database. Please wait ...');
 		this.mongoose.connect(this.connString);
 
-		var db = this.mongoose.connection;
-		db.on('error', console.error.bind(console, 'Error when connection to MongoDB:'));
-		db.once('open', function(){
-				console.log('[INFO] Connected to MongoDB at ds131511.mlab.com:31511/nvh_test');
+		this.mongoose.connection.on('error', console.error.bind(console, 'Error when connection to MongoDB:'));
+		this.mongoose.connection.once('open', function(){
+			console.log('[INFO] Connected to MongoDB simple-api-server with user quocbao');
 		});
+
+		var PageSchema = new this.mongoose.Schema({
+			slug : String,
+			title: String,
+			pathImg: String,
+			detail: String
+		});
+		this.Page = this.mongoose.model('Page', PageSchema);
+
+		var CommentSchema = new this.mongoose.Schema({
+			id : Number,
+			pathAvatar: String,
+			username: String,
+			datePost: String,
+			content: String});
+		this.Comment = this.mongoose.model('Comment', CommentSchema);
 	},
 
 	close: function()
@@ -74,7 +92,12 @@ var dao = {
 	*	out - return null nếu không có kết quả
 	*/
 	getService: function(slug){
-		return {slug: "design", title: "Design Research", pathImg: "/images/design.jpg", detail: "We help you better understand the needs and goals of your customers, uncovering key insighs that drive innovative design ideas" };
+		this.Page.find({'slug': slug}, function(err, data)
+		{
+			if (err) throw err;
+			return data;
+		});
+		//return {slug: "design", title: "Design Research", pathImg: "/images/design.jpg", detail: "We help you better understand the needs and goals of your customers, uncovering key insighs that drive innovative design ideas" };
 	},
 
 	/*
